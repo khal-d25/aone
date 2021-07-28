f4aa334df36e894fcef3bdd23d2a7306247bca18
khal-d25 <54522993+khal-d25@users.noreply.github.com>
2021-07-29 01:35:57 +0530

    home

--- index.js
+++ index.js
@@ -50,6 +50,23 @@ show.forEach((e)=>{
 })
 
 
+var lastScrollTop = 0;
+var scroll = 100;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      window.scrollBy(0,100)
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
+
+
+
 
 
 
